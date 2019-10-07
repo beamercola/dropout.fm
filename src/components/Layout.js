1afd6568d02ec52cde7dab3f2d5ed94794ebc68b
Beamer <beamercola@gmail.com>
2019-10-07 10:54:43 -0700

    Plausible

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,6 +21,16 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:image" content={`${withPrefix('/')}img/og-image.jpg`} />
+        <script type='text/javascript'>
+          {`
+            (function (w,d,s,o,f,js,fjs) {
+              w[o] = w[o] || function () { (w[o].q = w[o].q || []).push(arguments) };
+              js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
+              js.id = o; js.src = f; js.async = 1; fjs.parentNode.insertBefore(js, fjs);
+            }(window, document, 'script', 'plausible', 'https://plausible.io/js/p.js'));
+            plausible('page')
+          `}
+        </script>
       </Helmet>
       {children}
     </>
