de971881c691e4d4027f57da1f7f0772541acc28
Beamer <beamercola@gmail.com>
2019-10-27 17:24:43 -0700

    Spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Footer = () => (
-  <footer className="sm:w-96 text-xs my-8 px-3 dark:text-gray-600">
+  <footer className="sm:w-96 text-xs mt-8 px-3 dark:text-gray-600">
     say hi <a className="underline" href="https://twitter.com/beamercola">@beamercola</a>
   </footer>
 )
